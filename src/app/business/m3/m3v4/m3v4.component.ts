ead0e25ce47f0db96edf5978a8a87605b3352d33
Vetkdf <500sea@163.com>
2017-08-16 17:35:50 +0800

    update

--- src/app/business/m3/m3v4/m3v4.component.ts
+++ src/app/business/m3/m3v4/m3v4.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Inject } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Auxiliary } from '../../../common/constants/auxiliary';
 import * as d3 from 'd3';
 
@@ -9,7 +9,7 @@ import * as d3 from 'd3';
 })
 export class M3v4Component implements OnInit {
 
-  constructor(@Inject('title') private titleService) {
+  constructor( @Inject('title') private titleService) {
     this.titleService.setTitle("画中国地图");
   }
 
@@ -18,44 +18,44 @@ export class M3v4Component implements OnInit {
     this.showMap();
   }
 
-  showMap():void {
-    let width  = 1000;
-  	let height = 1000;
+  showMap(): void {
+    let width = 1000;
+    let height = 1000;
 
-  	let svg = d3.select("#container").append("svg")
-  	    .attr("width", width)
-  	    .attr("height", height)
-  	    .append("g")
-  	    .attr("transform", "translate(0,0)");
+    let svg = d3.select("#container").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(0,0)");
 
-  	let projection = d3.geoMercator()
-        .center([107, 31])
-        .scale(850)
-        .translate([width/2, height/2]);
+    let projection = d3.geoMercator()
+      .center([107, 31])
+      .scale(850)
+      .translate([width / 2, height / 2]);
 
-  	let path = d3.geoPath().projection(projection);
+    let path = d3.geoPath().projection(projection);
     let color = d3.scaleOrdinal(d3.schemeCategory20);
-  	d3.json("http://114.215.44.2:83/china.geojson", function(error, root) {
+    d3.json("http://114.215.44.2:83/china.geojson", function (error, root) {
 
-  	if (error) return console.error(error);
-  	//console.log(root.features);
+      if (error) return console.error(error);
+      //console.log(root.features);
 
-		svg.selectAll("path")
-			.data( root.features )
-			.enter()
-			.append("path")
-			.attr("stroke","#000")
-			.attr("stroke-width",1)
-			.attr("fill", function(d,i){ return color(i); })
-			.attr("d", path )
-			.on("mouseover",function(d,i){
-                d3.select(this).attr("fill","yellow");
-      })
-      .on("mouseout",function(d,i){
-          d3.select(this).attr("fill",color(i));
-      });
+      svg.selectAll("path")
+        .data(root.features)
+        .enter()
+        .append("path")
+        .attr("stroke", "#000")
+        .attr("stroke-width", 1)
+        .attr("fill", function (d, i) { return color(i); })
+        .attr("d", path)
+        .on("mouseover", function (d, i) {
+          d3.select(this).attr("fill", "yellow");
+        })
+        .on("mouseout", function (d, i) {
+          d3.select(this).attr("fill", color(i));
+        });
 
-  	});
+    });
   }
 
 }
--- src/app/business/services/getlist.ts
+++ src/app/business/services/getlist.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response ,RequestOptions} from '@angular/http';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/catch';
-import { PageBackList,Wijmo_PageBackList,PageBackContentSSM, PageBackContent_M2V2,PageBackContent_M2V3,BackNews} from '../../module/business/getlist';
+import { PageBackList, Wijmo_PageBackList, PageBackContentSSM, PageBackContent_M2V2, PageBackContent_M2V3, BackNews } from '../../module/business/getlist';
 import ConstantsList from '../../common/constants/config';
 import * as wjcCore from 'wijmo/wijmo';
 import { BackCode } from '../../module/business/formdata';
@@ -17,175 +17,175 @@ export class GetList extends BaseService {
     this.servicename = 'GetList-列表服务';
   }
 
-  public GetListPageBySSM(PageIndex:number,PageSize:number): Promise<Wijmo_PageBackList>{
+  public GetListPageBySSM(PageIndex: number, PageSize: number): Promise<Wijmo_PageBackList> {
     const url = `${ConstantsList.HOSTUser}yang-test/angular/pagelist/${PageIndex}/${PageSize}/`;
     return this.http.get(url)
-              .toPromise()
-              .then(res => {
-                let status:number = res.status;
-                if(status === 200){
-                  let PB:PageBackList = res.json() as PageBackList;
-                  let PBC:PageBackContentSSM[]  = PB.pageBackContent as PageBackContentSSM[];
-                  let Wijmo:Wijmo_PageBackList = new Wijmo_PageBackList();
-                  Wijmo.List = new wjcCore.ObservableArray();
-                  for (let i = 0; i < PBC.length; i++) {
-                      Wijmo.List.push({
-                          ID: PBC[i].id,
-                          P:  PBC[i].province,
-                          C:  PBC[i].city,
-                          A:  PBC[i].area,
-                      });
-                  }
-                  Wijmo.pageNews = PB.pageNews;
-                  return Wijmo;
-                }
-                else{
-                  console.error('服务端返回的 http status 错误 : ', status);
-                  return null;
-                }
-              })
-              .catch((error: any) => {return this.handleError('GetListPageBySSM',error);});
+      .toPromise()
+      .then(res => {
+        let status: number = res.status;
+        if (status === 200) {
+          let PB: PageBackList = res.json() as PageBackList;
+          let PBC: PageBackContentSSM[] = PB.pageBackContent as PageBackContentSSM[];
+          let Wijmo: Wijmo_PageBackList = new Wijmo_PageBackList();
+          Wijmo.List = new wjcCore.ObservableArray();
+          for (let i = 0; i < PBC.length; i++) {
+            Wijmo.List.push({
+              ID: PBC[i].id,
+              P: PBC[i].province,
+              C: PBC[i].city,
+              A: PBC[i].area,
+            });
+          }
+          Wijmo.pageNews = PB.pageNews;
+          return Wijmo;
+        }
+        else {
+          console.error('服务端返回的 http status 错误 : ', status);
+          return null;
+        }
+      })
+      .catch((error: any) => { return this.handleError('GetListPageBySSM', error); });
   }
 
-  public GetListPageBy_M2V2(PageIndex:number): Promise<Wijmo_PageBackList>{
+  public GetListPageBy_M2V2(PageIndex: number): Promise<Wijmo_PageBackList> {
     const url = `${ConstantsList.HOSTUser}/yang-test/angular/getbnindex/${PageIndex}/${ConstantsList.pageSize}/`;
     return this.http.get(url)
-              .toPromise()
-              .then(res => {
-                let status:number = res.status;
-                if(status === 200){
-                  let PB:PageBackList = res.json() as PageBackList;
-                  let PBC:PageBackContent_M2V2[]  = PB.pageBackContent as PageBackContent_M2V2[];
-                  let Wijmo:Wijmo_PageBackList = new Wijmo_PageBackList();
+      .toPromise()
+      .then(res => {
+        let status: number = res.status;
+        if (status === 200) {
+          let PB: PageBackList = res.json() as PageBackList;
+          let PBC: PageBackContent_M2V2[] = PB.pageBackContent as PageBackContent_M2V2[];
+          let Wijmo: Wijmo_PageBackList = new Wijmo_PageBackList();
 
-                  Wijmo.List = new wjcCore.ObservableArray();
-                  for (let i = 0; i < PBC.length; i++) {
-                      Wijmo.List.push({
-                          check:false,
-                          id: PBC[i].id,
-                          indexcode: PBC[i].indexcode,
-                          indexname: PBC[i].indexname,
-                          indexremark: PBC[i].indexremark,
-                          //isdel: PBC[i].isdel === 0 ? '启用':'禁用',
-                          //isdelandedit: PBC[i].isdelandedit === 0 ? '可编辑':'已锁定',
-                          isdel: PBC[i].isdel,
-                          isdelandedit:PBC[i].isdelandedit,
-                          button:PBC[i].id,
-                      });
-                  }
-                  Wijmo.pageNews = PB.pageNews;
-                  return Wijmo;
-                }
-                else{
-                  console.error('服务端返回的 http status 错误 : ', status);
-                  return null;
-                }
-              })
-              .catch((error: any) => { return this.handleError('GetListPageBy_M2V2',error);});
+          Wijmo.List = new wjcCore.ObservableArray();
+          for (let i = 0; i < PBC.length; i++) {
+            Wijmo.List.push({
+              check: false,
+              id: PBC[i].id,
+              indexcode: PBC[i].indexcode,
+              indexname: PBC[i].indexname,
+              indexremark: PBC[i].indexremark,
+              //isdel: PBC[i].isdel === 0 ? '启用':'禁用',
+              //isdelandedit: PBC[i].isdelandedit === 0 ? '可编辑':'已锁定',
+              isdel: PBC[i].isdel,
+              isdelandedit: PBC[i].isdelandedit,
+              button: PBC[i].id,
+            });
+          }
+          Wijmo.pageNews = PB.pageNews;
+          return Wijmo;
+        }
+        else {
+          console.error('服务端返回的 http status 错误 : ', status);
+          return null;
+        }
+      })
+      .catch((error: any) => { return this.handleError('GetListPageBy_M2V2', error); });
   }
 
-  public GetListPageBy_M2V3_List(): Promise<PageBackContent_M2V2[]>{
+  public GetListPageBy_M2V3_List(): Promise<PageBackContent_M2V2[]> {
     const url = `${ConstantsList.HOSTUser}/yang-test/angular/getbnindexlist/`;
     return this.http.get(url)
-              .toPromise()
-              .then(res => {  return res.json() as PageBackContent_M2V2[]; })
-              .catch((error: any) => {return this.handleError('GetListPageBy_M2V3_List',error);});
+      .toPromise()
+      .then(res => { return res.json() as PageBackContent_M2V2[]; })
+      .catch((error: any) => { return this.handleError('GetListPageBy_M2V3_List', error); });
   }
 
-  public GetListPageBy_M2V3(PageIndex:number,Type:string): Promise<Wijmo_PageBackList>{
+  public GetListPageBy_M2V3(PageIndex: number, Type: string): Promise<Wijmo_PageBackList> {
     const url = `${ConstantsList.HOSTUser}/yang-test/angular/getbasenews/${PageIndex}/${ConstantsList.pageSize}/${Type}/`;
     return this.http.get(url)
-              .toPromise()
-              .then(res => {
-                let status:number = res.status;
-                if(status === 200){
-                  let PB:PageBackList = res.json() as PageBackList;
-                  let PBC:PageBackContent_M2V3[] = PB.pageBackContent as PageBackContent_M2V3[]
-                  let Wijmo:Wijmo_PageBackList = new Wijmo_PageBackList();
+      .toPromise()
+      .then(res => {
+        let status: number = res.status;
+        if (status === 200) {
+          let PB: PageBackList = res.json() as PageBackList;
+          let PBC: PageBackContent_M2V3[] = PB.pageBackContent as PageBackContent_M2V3[]
+          let Wijmo: Wijmo_PageBackList = new Wijmo_PageBackList();
 
-                  Wijmo.List = new wjcCore.ObservableArray();
-                  for (let i = 0; i < PBC.length; i++) {
-                      Wijmo.List.push({
-                          check:false,
-                          id: PBC[i].id,
-                          indexcode: PBC[i].indexcode,
-                          code: PBC[i].code,
-                          name: PBC[i].name,
-                          //workid: PBC[i].workid,
-                          //worktime: PBC[i].worktime,
-                          remark: PBC[i].remark,
-                          //lastworkid: PBC[i].lastworkid,
-                          //updatetime: PBC[i].updatetime,
-                          orderid: PBC[i].orderid,
-                          isdel: PBC[i].isdel,
-                          isdelandedit: PBC[i].isdelandedit,
-                          //isdel: PBC[i].isdel === 0 ? '启用':'禁用',
-                          //isdelandedit: PBC[i].isdelandedit === 0 ? '可编辑':'已锁定',
-                          button:PBC[i].id,
-                      });
-                  }
-                  Wijmo.pageNews = PB.pageNews;
-                  return Wijmo;
-                }
-                else{
-                  console.error('服务端返回的 http status 错误 : ', status);
-                  return null;
-                }
-              })
-              .catch((error: any) => {return this.handleError('GetListPageBy_M2V3',error);});
+          Wijmo.List = new wjcCore.ObservableArray();
+          for (let i = 0; i < PBC.length; i++) {
+            Wijmo.List.push({
+              check: false,
+              id: PBC[i].id,
+              indexcode: PBC[i].indexcode,
+              code: PBC[i].code,
+              name: PBC[i].name,
+              //workid: PBC[i].workid,
+              //worktime: PBC[i].worktime,
+              remark: PBC[i].remark,
+              //lastworkid: PBC[i].lastworkid,
+              //updatetime: PBC[i].updatetime,
+              orderid: PBC[i].orderid,
+              isdel: PBC[i].isdel,
+              isdelandedit: PBC[i].isdelandedit,
+              //isdel: PBC[i].isdel === 0 ? '启用':'禁用',
+              //isdelandedit: PBC[i].isdelandedit === 0 ? '可编辑':'已锁定',
+              button: PBC[i].id,
+            });
+          }
+          Wijmo.pageNews = PB.pageNews;
+          return Wijmo;
+        }
+        else {
+          console.error('服务端返回的 http status 错误 : ', status);
+          return null;
+        }
+      })
+      .catch((error: any) => { return this.handleError('GetListPageBy_M2V3', error); });
   }
 
   //======================================================
 
-  public GetSequenceCode(Type:number,IsAdd:number):Promise<string> {
+  public GetSequenceCode(Type: number, IsAdd: number): Promise<string> {
     const url = `${ConstantsList.HOSTUser}/yang-test/angular/getsequencecode/${Type}/${IsAdd}/`;
     return this.http.get(url)
-              .toPromise()
-              .then(res => {
-                let status:number = res.status;
-                if(status === 200){
-                  let Back = res.json() as BackNews;
-                  return Back.backNews;
-                }
-                else{
-                  console.error('服务端返回的 http status 错误 : ', status);
-                  return null;
-                }
-              })
-              .catch((error: any) => {return this.handleError('GetSequenceCode',error);});
+      .toPromise()
+      .then(res => {
+        let status: number = res.status;
+        if (status === 200) {
+          let Back = res.json() as BackNews;
+          return Back.backNews;
+        }
+        else {
+          console.error('服务端返回的 http status 错误 : ', status);
+          return null;
+        }
+      })
+      .catch((error: any) => { return this.handleError('GetSequenceCode', error); });
   }
 
   //======================================================
 
-  public Form_M2V2(postvalue:PageBackContent_M2V2,IsAdd:boolean): Promise<BackCode> {
+  public Form_M2V2(postvalue: PageBackContent_M2V2, IsAdd: boolean): Promise<BackCode> {
     const url = `${ConstantsList.HOSTUser}/yang-test/angular/form_m2v2/`;
     let headers = ConstantsList.headers;
     let options = new RequestOptions({ headers: headers });
-    if(IsAdd) {
+    if (IsAdd) {
       return this.http.post(url, postvalue, options).toPromise()
-      .then((res) => { return res.json() as BackCode; })
-      .catch((error: any) => {return this.handleError('Form_M2V2',error);});
+        .then((res) => { return res.json() as BackCode; })
+        .catch((error: any) => { return this.handleError('Form_M2V2', error); });
     }
     else {
       return this.http.put(url, postvalue, options).toPromise()
-      .then((res) => { return res.json() as BackCode; })
-      .catch((error: any) => {return this.handleError('Form_M2V2',error);});
+        .then((res) => { return res.json() as BackCode; })
+        .catch((error: any) => { return this.handleError('Form_M2V2', error); });
     }
   }
 
-  public Form_M2V3(postvalue:PageBackContent_M2V3,IsAdd:boolean): Promise<BackCode> {
+  public Form_M2V3(postvalue: PageBackContent_M2V3, IsAdd: boolean): Promise<BackCode> {
     const url = `${ConstantsList.HOSTUser}/yang-test/angular/form_m2v3/`;
     let headers = ConstantsList.headers;
     let options = new RequestOptions({ headers: headers });
-    if(IsAdd) {
+    if (IsAdd) {
       return this.http.post(url, postvalue, options).toPromise()
-      .then((res) => { return res.json() as BackCode; })
-      .catch((error: any) => {return this.handleError('Form_M2V3',error);});
+        .then((res) => { return res.json() as BackCode; })
+        .catch((error: any) => { return this.handleError('Form_M2V3', error); });
     }
     else {
       return this.http.put(url, postvalue, options).toPromise()
-      .then((res) => { return res.json() as BackCode; })
-      .catch((error: any) => {return this.handleError('Form_M2V3',error);});
+        .then((res) => { return res.json() as BackCode; })
+        .catch((error: any) => { return this.handleError('Form_M2V3', error); });
     }
   }
 
